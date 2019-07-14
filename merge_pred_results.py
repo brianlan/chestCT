b2b86b004438bb5cd53d06eb490d3423836911bc
brianlan <brianlanbo@gmail.com>
2019-07-15 00:47:21 +0800

    implemented match_detections_into_groups

--- merge_pred_results.py
+++ merge_pred_results.py
@@ -1,38 +1,83 @@
 import argparse
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from pathlib import Path
 
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 from src.dataset import Meta
-from src.utils import merge_slices_of_patient
+from src.utils import merge_slices_of_patient, calc_overlaps
 
 
+np.set_printoptions(suppress=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--objinfo-path", type=Path, required=True)
 parser.add_argument("--meta-dir", type=Path, required=True)
 parser.add_argument("--output-file-path", type=Path, required=True)
+parser.add_argument("--match-iou-thresh", type=float, default=0.3)
 args = parser.parse_args()
 
 
-def read_pred_lesions():
+def read_predictions():
     """Only returns detected lesions (foreground) """
     with open(args.objinfo_path, 'r') as f:
         lines = [l.strip().split(' ') for l in f]
     sorted_lines = sorted(lines, key=lambda x: x[0])
-    single_slice_lesions = OrderedDict()
+    single_slice_lesions = defaultdict(OrderedDict)
     for l in sorted_lines:
         if int(l[1]) > 0:
             patient_id, slice_id = l[0].split('_')
-            single_slice_lesions[patient_id] = OrderedDict()
-            single_slice_lesions[patient_id][int(slice_id)] = np.array(l[2:]).reshape(int(l[1]), 6)
+            single_slice_lesions[patient_id][int(slice_id)] = np.array(l[2:], dtype=np.float32).reshape(int(l[1]), 6)
     return single_slice_lesions
 
 
+def match_detections_into_groups(slice_partition):
+    # initialize groups with the first slice detections
+    slice_iter = iter(slice_partition.keys())
+    first_slice_id = next(slice_iter)
+    groups = [det[None, ...] for det in slice_partition[first_slice_id]]
+
+    # go through the next slice to find close dets and put them into the same group
+    for slice_id in slice_iter:
+        latest_idx, latest_added_dets = zip(*[(i, g[-1]) for i, g in enumerate(groups) if g[-1].sum() != 0.0])
+        overlaps = calc_overlaps(slice_partition[slice_id], latest_added_dets)
+        assignment = linear_sum_assignment(1 - overlaps)
+        for s_id, g_id in zip(assignment[0], assignment[1]):
+            r_g_id = latest_idx[g_id]  # real g_id
+            if overlaps[s_id, g_id] > args.match_iou_thresh:
+                groups[r_g_id] = np.concatenate((groups[r_g_id], slice_partition[slice_id][None, s_id]), axis=0)
+            else:
+                groups[r_g_id] = np.concatenate((groups[r_g_id], np.zeros((1, 6))), axis=0)
+                groups.append(slice_partition[slice_id][None, s_id])
+
+    return groups
+
+
+def process_slice_partition(slice_partition, meta):
+    groups = match_detections_into_groups(slice_partition)
+    a = 19
+
+
+def process_patient(patient_id, pred, meta):
+    """the lesions of a patient will be parted into several partitions with consecutive slice_id,
+    then, each detected bboxes at different slices with high IOU will be considered as a group.
+    Finally the bboxes in a group will be merged as a lesion output.
+    """
+    prev = None
+    partition = OrderedDict()
+    for slice_id, dets in pred.items():
+        if prev is not None and prev + 1 != slice_id:
+            process_slice_partition(partition, meta)
+            partition = OrderedDict({slice_id: dets})
+        else:
+            partition[slice_id] = dets
+        prev = slice_id
+
+
 def merge_pred_results():
-    pred_lesions = read_pred_lesions()
+    pred_lesions = read_predictions()
     for patient_id, pred in pred_lesions.items():
-        merge_slices_of_patient(patient_id, pred, Meta.from_path(args.meta_dir / f"{patient_id}.mhd"))
+        process_patient(patient_id, pred, Meta.from_path(args.meta_dir / f"{patient_id}.mhd"))
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,172 @@
+import math
+from collections import Iterable
+import operator
+
 import numpy as np
+
+
+class _Coord(list):
+    def __init__(self, coords):
+        super(_Coord, self).__init__(coords)
+
+    def __repr__(self):
+        ele_fmt = '{:.5f}' if any([isinstance(c, float) for c in self]) else '{:d}'
+        return '({})'.format(', '.join([ele_fmt.format(c) for c in self]))
+
+    def _adaptive_op(self, other, op):
+        """this div function adapts to both scalar and iterable with the same length"""
+        if isinstance(other, int) or isinstance(other, float):
+            return _Coord([op(c, other) for c in self])
+
+        if isinstance(other, Iterable) and len(other) == len(self):
+            return _Coord([op(s, o) for s, o in zip(self, other)])
+
+    def __mul__(self, other):
+        return self._adaptive_op(other, operator.mul)
+
+    def __sub__(self, other):
+        return self._adaptive_op(other, operator.sub)
+
+    def __add__(self, other):
+        return self._adaptive_op(other, operator.add)
+
+    def __truediv__(self, other):
+        return self._adaptive_op(other, operator.truediv)
+
+    def __floordiv__(self, other):
+        return self._adaptive_op(other, operator.floordiv)
+
+
+class Coord2D(_Coord):
+    def __init__(self, coords):
+        assert len(coords) == 2
+        super(Coord2D, self).__init__(coords)
+
+
+class Coord4D(_Coord):
+    def __init__(self, coords):
+        assert len(coords) == 4
+        super(Coord4D, self).__init__(coords)
+
+
+class BBox:
+    def __init__(self, left, top, right, bottom):
+        self.coord = Coord4D([float(c) for c in [left, top, right, bottom]])
+
+    @classmethod
+    def from_xml_element(cls, root):
+        return BBox(root.find('xmin').text, root.find('ymin').text, root.find('xmax').text, root.find('ymax').text)
+
+    @classmethod
+    def from_json(cls, data):
+        return BBox(data['xmin'], data['ymin'], data['xmax'], data['ymax'])
+
+    @classmethod
+    def from_xywh(cls, x, y, w, h):
+        half_width, half_height = w / 2, h / 2
+        return BBox(x - half_width, y - half_height, x + half_width, y + half_height)
+
+    @property
+    def left(self):
+        return self.coord[0]
+
+    @property
+    def top(self):
+        return self.coord[1]
+
+    @property
+    def right(self):
+        return self.coord[2]
+
+    @property
+    def bottom(self):
+        return self.coord[3]
+
+    @property
+    def width(self):
+        return max(0, self.right - self.left + 1)
+
+    @property
+    def height(self):
+        return max(0, self.bottom - self.top + 1)
+
+    @property
+    def size(self):
+        return Coord2D([self.width, self.height])
+
+    @property
+    def center(self):
+        return Coord2D((self.left + self.right, self.top + self.bottom)) / 2
+
+    def hscale(self, scalar, in_place=False):
+        """scale horizontally"""
+        if not in_place:
+            return BBox(self.coord[0] * scalar, self.coord[1], self.coord[2] * scalar, self.coord[3])
+
+        self.coord[0] *= scalar
+        self.coord[2] *= scalar
+        return self
+
+    def vscale(self, scalar, in_place=False):
+        """scale vertically"""
+        if not in_place:
+            return BBox(self.coord[0], self.coord[1] * scalar, self.coord[2], self.coord[3] * scalar)
+
+        self.coord[1] *= scalar
+        self.coord[3] *= scalar
+        return self
+
+    def gscale(self, scalars, in_place=False):
+        """global scale"""
+        assert len(scalars) == 2
+
+        if not in_place:
+            return BBox(self.coord[0] * scalars[0], self.coord[1] * scalars[1],
+                        self.coord[2] * scalars[0], self.coord[3] * scalars[1])
+
+        return self.hscale(scalars[0]).vscale(scalars[1])
+
+    def clamp(self, x_max, y_max, x_min=0, y_min=0, in_place=False):
+        assert x_max > x_min and y_max > y_min
+
+        if not in_place:
+            return BBox(max(x_min, min(self.left, x_max)),
+                        max(y_min, min(self.top, y_max)),
+                        max(x_min, min(self.right, x_max)),
+                        max(y_min, min(self.bottom, y_max)))
+
+        self.coord[0] = max(x_min, min(self.left, x_max))
+        self.coord[1] = max(y_min, min(self.top, y_max))
+        self.coord[2] = max(x_min, min(self.right, x_max))
+        self.coord[3] = max(y_min, min(self.bottom, y_max))
+        return self
+
+    def area(self):
+        return self.width * self.height
+
+    def is_empty(self):
+        return self.left == self.right or self.top == self.bottom
+
+    def __repr__(self):
+        return ', '.join(['{:.5f}'.format(c) for c in self.coord])
+
+    def __eq__(self, other):
+        return math.fabs(self.coord[0] - other.coord[0]) < 1e-7 and \
+               math.fabs(self.coord[1] - other.coord[1]) < 1e-7 and \
+               math.fabs(self.coord[2] - other.coord[2]) < 1e-7 and \
+               math.fabs(self.coord[3] - other.coord[3]) < 1e-7
+
+    def __sub__(self, other):
+        """Intersection between self and the other BBox. This operation should obey commutative law."""
+        intsec_area_left = max(self.left, other.left)
+        intsec_area_right = min(self.right, other.right)
+        intsec_area_top = max(self.top, other.top)
+        intsec_area_bottom = min(self.bottom, other.bottom)
+        return BBox(intsec_area_left, intsec_area_top, intsec_area_right, intsec_area_bottom)
+
+    def enclosed_by(self, other):
+        return self.left >= other.left and self.right <= other.right and \
+               self.top >= other.top and self.bottom <= other.bottom
 
 
 def get_indices(ind_path, root_dir, suffix):
@@ -70,3 +238,17 @@ def cuboid_pix_2_real(coord_x, coord_y, coord_z, diameter_x, diameter_y, diamete
         diameter_x * meta.element_spacing[0], \
         diameter_y * meta.element_spacing[1], \
         diameter_z * meta.element_spacing[2]
+
+
+def iou(a: BBox, b: BBox):
+    i = (a - b).area()
+    u = a.area() + b.area() - (a - b).area()
+    return i / (u + 1e-7)
+
+
+def calc_overlaps(dets_a, dets_b):
+    overlaps = np.zeros((len(dets_a), len(dets_b)))
+    for i, a in enumerate(dets_a):
+        for j, b in enumerate(dets_b):
+            overlaps[i, j] = (a[0] == b[0]) * iou(BBox(*a[1:5]), BBox(*b[1:5]))
+    return overlaps
